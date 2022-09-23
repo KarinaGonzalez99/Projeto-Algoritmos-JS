c4fa8fcb3584171921f1ae890c64decbb0da725c
Kaka <kaahgonzalez99@gmail.com>
2022-09-23 18:59:44 -0300

    Deixando o código mais limpo

--- exer3.js
+++ exer3.js
@@ -1,5 +1,6 @@
 // 3 - Desenvolva um algoritmo que simule o jogo "pedra, papel e tesoura" levando em consideração que são apenas duas pessoas jogando. Imprima o resultado no formato:
 // "Player 1 won" ou "A Ties" ou "Player 2 won".
+
 let p1 = 'Pedra';
 let p2 = 'Tesoura';
 
--- exer4.js
+++ exer4.js
@@ -1,6 +1,7 @@
 // 4 - Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
 // Imprima no console seguindo o exemplo: "A pessoa é maior de idade".
 // PS. Considere que uma pessoa com 18 anos ou mais é uma pessoa maior de idade.
+
 const idade= 18;
 if(idade >= 18){
     console.log('A pessoa é maior de idade');
