81f232f95c06de0c92a92832ac7e2eadc5036a45
Kaka <kaahgonzalez99@gmail.com>
2022-09-23 18:28:36 -0300

    Finalização dos exercicios

--- exer3.js
+++ exer3.js
@@ -1,2 +1,15 @@
 // 3 - Desenvolva um algoritmo que simule o jogo "pedra, papel e tesoura" levando em consideração que são apenas duas pessoas jogando. Imprima o resultado no formato:
 // "Player 1 won" ou "A Ties" ou "Player 2 won".
+let p1 = 'Pedra';
+let p2 = 'Tesoura';
+
+if (p1 === p2) {
+    console.log('A Ties!')
+} 
+else if ( p1 === 'Pedra' && p2 === 'Tesoura' || p1 === 'Papel' && p2 === 'Pedra' || p1 === 'Tesoura' && p2 === 'Papel') {
+    console.log('Player 1 won')
+}
+else {
+    console.log('Player 2 won')
+}
+
--- exer5.js
+++ exer5.js
@@ -1,2 +1,16 @@
 // 5 - Desenvolva um algoritmo que recebe a idade de 3 pessoas e imprime quem é a pessoa mais nova. Exiba o resultado no seguinte formato:
 // "Pessoa" é a pessoa mais nova.
+
+let pessoa1 = 12;
+let pessoa2 = 15;
+let pessoa3 = 1;
+
+if (pessoa1 < pessoa2 && pessoa1 < pessoa3){
+    console.log('Pessoa 1 é a pessoa mais nova');
+}
+else if (pessoa2 < pessoa1 && pessoa2 < pessoa3){
+    console.log('Pessoa 2 é a mais nova');
+}
+else {
+    console.log('Pessoa 3 é a mais nova');
+}
