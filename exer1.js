0ac661b08a484ae17f4dfc92d02710739ba362ab
Kaka <kaahgonzalez99@gmail.com>
2022-09-23 18:04:59 -0300

    concluindo alguns exercicios

--- exer1.js
+++ exer1.js
@@ -1,2 +1,8 @@
 // 1 - Desenvolva um algoritmo que calcula a soma dos números 1 até 50 usando a estrutura condicional "for". Retorne a resposta no seguinte formato:
 // A soma total de 1 até 50 é: X
+
+let result = 0;
+for (let i = 0; 1 < 50; i++){
+    result = result +1
+    console.log(result)
+}
--- exer2.js
+++ exer2.js
@@ -1,3 +1,16 @@
 // 2 - Desenvolva um algoritmo que conte quantos números, em um intervalo entre dois números, são divisíveis por 3. Exemplo: 1 e 20.
 // Caso a quantidade seja igual a 50, exiba uma mensagem secreta (pode inventar uma). 
 // Não conta pra ninguém, mas no intervalo entre 2 e 150 existem 50 números divisíveis por 3 ;)
+
+let resultado = 0;
+for(let i = 2; i < 150; i++){
+    if (i%3 === 0) {
+        result = result + 1
+}
+console.log(resultado)
+    if(result === 50){
+        console.log('Passamos por mais uma!!!')
+    }
+}
+
+
